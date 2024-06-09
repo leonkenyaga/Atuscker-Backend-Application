ea186ebdf284014fa5096771264ddb5e6f4ef2f7
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-06-09 19:24:53 +0300

    configured new urls

--- Atuscker/AtusckerWebapp/urls.py
+++ Atuscker/AtusckerWebapp/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("", views.index, name="index"),
     path("product_list", views.product_list, name="product_list"),
-    path("specific_product", views.specific_product, name="specific_product")
+    path('products/<int:pk>/', views.product_detail) 
 ]
