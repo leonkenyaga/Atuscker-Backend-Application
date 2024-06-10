9334e60aad6b846548a790b31d4e83a8e366a460
Leon Kenyaga <wolfofwallstreet@kali.Leon.com>
2024-06-10 17:20:42 +0300

    configured productimagelist url

--- Atuscker/AtusckerWebapp/urls.py
+++ Atuscker/AtusckerWebapp/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path("product_list", views.product_list, name="product_list"),
-    path('products/<int:pk>/', views.product_detail) 
+    path('products/<int:pk>/', views.product_detail),
+    path('productimage_list', views.productimage_list)
 ]
